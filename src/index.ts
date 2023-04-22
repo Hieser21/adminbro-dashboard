12ae7afd32289987655f890dd07ae42443d67001
Reigin16 <128984477+Reigin16@users.noreply.github.com>
2023-04-22 18:41:52 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@ import express from 'express'
 import bcrypt from 'bcrypt'
 import Users from './db/Users'
 import mongoose from 'mongoose'
+import AdminJS from 'adminjs'
 import AdminJSExpress from '@adminjs/express'
 import adminBroOptions from './adminbro-options'
 import {default as MongoStore} from 'connect-mongo'
 require('dotenv').config()
 const sessionStore = MongoStore.create({
-  mongoUrl: process.env.MONGO_URI,
+  mongoUrl: 'mongodb+srv://Bot:rattlesnake20@cluster0.ehnrp.mongodb.net/mern?retryWrites=true&w=majority',
   ttl: 14 * 24 * 60 * 60,
   autoRemove: 'native'
 })
 
 const cookie = process.env.COOKIE_PASSWORD
-const router = AdminJSExpress.buildAuthenticatedRouter(adminBroOptions, {
+const router = AdminJSExpress.buildAuthenticatedRouter(new AdminJS(adminBroOptions), {
   authenticate: async function(email: any, password: any){
     const user = await Users.findOne({ email })
     if (user) {
@@ -26,30 +27,33 @@ const router = AdminJSExpress.buildAuthenticatedRouter(adminBroOptions, {
   },
   cookiePassword: `${cookie}`
 }, null, {
-  store: sessionStore,
-  resave: true,
   saveUninitialized: true,
+  resave: true,
   secret: 'sessionsecret',
+  
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-  }
-},)
+  httpOnly: process.env.NODE_ENV !== 'prodcution',
+    secure: process.env.NODE_ENV !== 'production',
+    maxAge: 24 * 60 * 14 * 60
+  },
+  store: sessionStore
+},
+
+)
 
 const app = express()
-app.use(adminBroOptions.options.rootPath, router)
+app.use(adminBroOptions.rootPath, router)
 app.use("/asset", express.static("public"))
 
 
 app.get('/', function(req, res) { res.redirect('/admin')})
 const run = async () => {
-  await mongoose.connect(`${process.env.MONGO_URI}`, {
+  await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehnrp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true
-  }).then(function () {
-    console.log('DB connected')
-  })
+  }).then(function(value){ console.log(value)})
 
   await app.listen(8080)
 }
